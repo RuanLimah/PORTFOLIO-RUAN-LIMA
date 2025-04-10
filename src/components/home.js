5320253354ff288f101d25788cd28494cc1ea46f
ruanlimah <ruanlimah@hotmail.com>
2025-04-10 08:13:18 -0300

    alterações nos componentes

--- src/components/home.js
+++ src/components/home.js
@@ -91,7 +91,7 @@ function Home() {
             </li>
             <li className="home-item">
               <a 
-                href="https://drive.google.com/file/d/1KPeMVKDoqVgR8ZIbft9woQGPRTVCfAaI/view?usp=sharing"
+                href="https://drive.google.com/file/d/1bAmfa8msisE9g0dqfOBIPSF6lnV8Vgdq/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 onClick={() => setMenuOpen(false)}
